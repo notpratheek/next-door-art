bc0057c09cc24c9bd30553e1afb8409b8b51248d
Pychimp <pratheek.i@gmail.com>
2012-11-21 15:27:40 +0530

    subtle devel

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-    var can = $("#art")[0].getContext("2d");
-    can.fillStyle= "#ff0000";
-    can.fillRect(15,15,50,75);
+    var tall = $(document).height();
+    var fat = $(document).width();
+    alert(tall + " and " + fat);
+    // var can = $("#art")[0].getContext("2d");
+    // can.fillStyle= "#ff0000";
+    // can.fillRect(15,15,50,75);
 });
