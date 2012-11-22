6f1ff823364187076408530a132f95819be84f75
Pychimp <pratheek.i@gmail.com>
2012-11-22 08:58:35 -0500

    finally worked

--- main.coffee
+++ main.coffee
@@ -1,29 +1,6 @@
 $(document).ready ->
-    ###
-    can = $("#art")[0].getContext("2d")
-    can.height = $(window).height()
-    can.width= $(window).width()
-    alert(can.width + " and " + can.height)
-    can.fillStyle = "#bada55"
-    can.fillRect(0,0,can.width,can.height)
-    ###
-
-    context = $("#art")[0].getContext("2d")
-    context.beginPath()
-    context.moveTo(188, 130)
-    context.bezierCurveTo(140, 10, 388, 10, 388, 170)
-    context.lineWidth = 10
-    context.strokeStyle = 'black'
-    context.stroke()
-
- 
- 
-
-    
-    ###
-    ctx.width = window.innerWidth
-    ctx.height = window.innerHeight
-    alert(ctx.width + " and " + ctx.height)
-    ctx.fillStyle = "#BADA55"
-    ctx.fillRect(0, 0, ctx.width, ctx.height)
-    ###
+    ctx = $("#art")[0].getContext("2d")
+    ctx.fillStyle = "#000000"
+    fat = $(document).width()
+    tall = $(document).height()
+    ctx.fillRect(0, 0, fat, tall)
--- main.js
+++ main.js
@@ -2,31 +2,12 @@
 (function() {
 
   $(document).ready(function() {
-    /*
-        can = $("#art")[0].getContext("2d")
-        can.height = $(window).height()
-        can.width= $(window).width()
-        alert(can.width + " and " + can.height)
-        can.fillStyle = "#bada55"
-        can.fillRect(0,0,can.width,can.height)
-    */
-
-    var context;
-    context = $("#art")[0].getContext("2d");
-    context.beginPath();
-    context.moveTo(188, 130);
-    context.bezierCurveTo(140, 10, 388, 10, 388, 170);
-    context.lineWidth = 10;
-    context.strokeStyle = 'black';
-    return context.stroke();
-    /*
-        ctx.width = window.innerWidth
-        ctx.height = window.innerHeight
-        alert(ctx.width + " and " + ctx.height)
-        ctx.fillStyle = "#BADA55"
-        ctx.fillRect(0, 0, ctx.width, ctx.height)
-    */
-
+    var ctx, fat, tall;
+    ctx = $("#art")[0].getContext("2d");
+    ctx.fillStyle = "#000000";
+    fat = $(document).width();
+    tall = $(document).height();
+    return ctx.fillRect(0, 0, fat, tall);
   });
 
 }).call(this);
